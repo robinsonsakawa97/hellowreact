cd5a18f2cc89693003a66c8fe8269a19f19c72ef
robinsonsakawa97 <44343508+robinsonsakawa97@users.noreply.github.com>
2021-10-18 23:05:05 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+import Reacrt from "react"
+import ReactDom from "react-dom"
+
+const alex=<form>the best
+<header>the man</header>
+<label>name</label>
+<input name="ronis"></input><break></break>
+<label>Email</label>
+<input type="e" name="email"></input>
+<break>
+</break>
+<label>password</label>
+<input type="password"></input>
+</form>
+ReactDom.render(alex, document.getElementById('root')); 
